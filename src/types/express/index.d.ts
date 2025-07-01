7de85f6d6e3c6a02d8b413b6391ff971b603ba13
fadlyian <mansyuraw24@gmail.com>
2025-07-02 00:18:56 +0700

    #feat: update Express Request interface to use userId and include email field

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,8 +1,9 @@
 declare namespace Express{
     export interface Request {
         user?: {
-            id:string;
+            userId:string;
             name: string;
+            email: string;
         }
     }
 }
