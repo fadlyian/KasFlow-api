6a2bbc631c11449a955c49ba6839b8addc889692
fadlyian <mansyuraw24@gmail.com>
2025-07-01 23:27:57 +0700

    #feat: remove initial_balance column from pockets table and update schema

---
+++ prisma/migrations/20250701162520_remove_column_initial_balance/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `initial_balance` on the `pockets` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "pockets" DROP COLUMN "initial_balance";
